1a458c2949e517de881f2d9f0af1ea515491f347
Spartak142 <mishav147@gmail.com>
2017-02-25 20:28:50 +0100

    Update sound.c

--- sound.c
+++ sound.c
@@ -8,3 +8,24 @@ void SOUND_init() {
 void SOUND_beep(int freq, int length) {
 
 }
+void soundfailure(){
+ SOUND_beep (200, 2);
+ SOUND_beep (400, 1);
+ SOUND_beep (200, 2);
+ SOUND_beep (400, 1);
+}
+void soundunlock(){                     //#Mary had a little lamb
+ SOUND_beep (659.25,2); //e
+ SOUND_beep (587.33,2); //d
+ SOUND_beep (554.00,2); //c
+ SOUND_beep (587.33,2);
+ SOUND_beep (659.25,2);
+ SOUND_beep (659.25,2);
+ SOUND_beep (659.25,2);
+ SOUND_beep (587.33,2);
+ SOUND_beep (587.33,2);  
+ SOUND_beep (587.33,2);  
+ SOUND_beep (659.25,2);
+ SOUND_beep (659.25,2);
+ SOUND_beep (659.25,2);
+}
