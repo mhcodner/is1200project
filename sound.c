df1e3a21f3d6bae1b7a4d268cd423177832fe23d
Mikhail <mishav147@gmail.com>
2017-02-27 14:52:15 +0100

    Update sound.c

--- sound.c
+++ sound.c
@@ -14,7 +14,7 @@ void SOUND_init() {
     
 }
 // makes the sound
-void SOUND_beep(int freq, int length) {
+void SOUND_beep(double freq, int length) {
 	if (freq == 0){
 	 	PR3 = 0;
 		OC1CON = 0x000E;    			// turn on output compare 1 module 
@@ -32,10 +32,16 @@ void soundfail(){
  SOUND_beep (200, 2);
  SOUND_beep (400, 1);
 }
+void soundfailR(){                      //Wah Wah Waaaah
+SOUND_beep (659.25,2);
+SOUND_beep (622.25,2);	
+SOUND_beep (587.33,2);
+SOUND_beep (554.37,4);
+}
 void soundunlock(){                     //Plays during the unlock #Mary had a little lamb
  SOUND_beep (659.25,2); //e
  SOUND_beep (587.33,2); //d
- SOUND_beep (554.00,2); //c
+ SOUND_beep (523.23,2); //c
  SOUND_beep (587.33,2);
  SOUND_beep (659.25,2);
  SOUND_beep (659.25,2);
@@ -46,6 +52,20 @@ void soundunlock(){                     //Plays during the unlock #Mary had a li
  SOUND_beep (659.25,2);
  SOUND_beep (659.25,2);
  SOUND_beep (659.25,2);
+}
+
+void soundunlockR(){                  // Starwars theme?
+ SOUND_beep (659.25,2); 
+ SOUND_beep (587.33,2);
+ SOUND_beep (659.25,2); 
+ SOUND_beep (587.33,2);
+ SOUND_beep (659.25,2);
+ SOUND_beep (987.77,2); 
+ SOUND_beep (587.33,2);
+ SOUND_beep (523.23,2); 
+ SOUND_beep (880.00,2);
+ SOUND_beep (523.23,2);
+ 
 }
 //Outputs different sound for different button combos, we can adjust frequencies later.
 void btnpress (int getbtns){
