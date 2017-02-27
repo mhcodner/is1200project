72d995a2d4490d5b256dbf4e392fd52534e0627a
Mikhail <mishav147@gmail.com>
2017-02-27 14:05:41 +0100

    Update sound.c

--- sound.c
+++ sound.c
@@ -1,13 +1,30 @@
 #include <pic32mx.h>
 #include "sound.h"
 
+#define TMR3PERIOD ((80000000 / 256) / 10)
+#define T3CON PIC32_R(0x0A00)
+#define T3CONSET PIC32_R(0x0A08)
+#define TMR3 PIC32_R(0x0A10)
+#define PR3 PIC32_R(0x0A20)
+
 void SOUND_init() {
-
+    OC1CON = 0b01110;    				// set output compare 1 module to PMW mode
+    T3CONSET = 0x8000;      			// start the timer 15th bit
+    OC1CON |= 0x8000;    				// turn on output compare 1 module 
+    
 }
-
+// makes the sound
 void SOUND_beep(int freq, int length) {
-
+	if (freq == 0){
+	 	PR3 = 0;
+		OC1CON = 0x000E;    			// turn on output compare 1 module 
+	}
+	PR3 = ((80000000 / 64)) / freq;
+	
+// not sure if this works, and still gotta learn how to make it buzz for a certain time.
+// shall we do it like in lab 3 with time out count?	
 }
+
 // Plays in case of a fail
 void soundfail(){
  SOUND_beep (200, 2);
@@ -15,7 +32,7 @@ void soundfail(){
  SOUND_beep (200, 2);
  SOUND_beep (400, 1);
 }
-void soundunlock(){                     //Plays during the unluck#Mary had a little lamb
+void soundunlock(){                     //Plays during the unlock #Mary had a little lamb
  SOUND_beep (659.25,2); //e
  SOUND_beep (587.33,2); //d
  SOUND_beep (554.00,2); //c
